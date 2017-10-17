de846067a0ae8f1d1d2f53bb54435f77b510e30a
Ahmed Nour <matrixamn@gmail.com>
2017-10-18 00:42:42 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,35 +2,29 @@
 
 ## create matrix and cache it
 
-makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setInverse <- function(solve) m <<- solve
-  getInverse <- function() m
-  
-  list(set = set, get = get,
-       setInverse = setInverse,
-       getInverse = getInverse)
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
+ 
 
-## Computes the inverse of makeCacheMatrix 
-## If inverse exists already then retrieve from cache
-
-cacheSolve <- function(x, ...) {
-  ##  Check to see if inverse already exists   
-  m <- x$getInverse()
-  ## Yes - get inverse from cache & skip comp    
-  if (!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  ## No - compute inverse      
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setInverse(m)
-  m
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
